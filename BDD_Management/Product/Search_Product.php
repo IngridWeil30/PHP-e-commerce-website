024be764c47bf21b51d3cc6e59c1b8c56ea445db
arthur.mariani@coding-academy.fr <arthur.thomas.mariani@gmail.com>
2018-03-21 19:01:01 +0100

    index

---
+++ BDD_Management/Product/Search_Product.php
@@ -0,0 +1,30 @@
+<?php
+function search_product($db, $cat, $name, $min_price, $max_price,$order,$way){
+  $data = [
+  'cat' => $cat,
+  'name' => $name,
+  'min_price' => $min_price,
+  'max_price' => $max_price,
+  'order' => $order,
+  'way' => $way
+  ];
+
+  $stmt = $db->prepare("SELECT * FROM products WHERE category_id = :cat
+    AND name like :name
+    AND price >=:min_price
+    AND price <= :max_price
+  ORDER BY :order :way"
+  );
+  $stmt->execute($data);
+
+  $element = $stmt->fetchAll(PDO::FETCH_OBJ);
+  $i = 0;
+  if ($element){
+    foreach ($element as $product) {
+      $productstable[$i]=$product;
+      $i++;
+    }
+  }
+  return($productstable);
+}
+?>
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
 include "BDD_Management/Product/get_products.php";
+include "BDD_Management/Product/Search_product.php";
+include "BDD_Management/Categories/get_categories.php";
 include "BDD_Management/connect_db.php";
 include "PHP_Generated/Generate_product.php";
 session_start();
 $db = connect_db();
-
-if (isset($_GET['logout'])) {
-  session_destroy();
-  unset($_SESSION['username']);
-  header("location: User/Login_Register/login.php");
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -19,51 +14,107 @@ if (isset($_GET['logout'])) {
     <title>Bootstrap Example</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="index.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+      $( function() {
+        $( "#slider-range" ).slider({
+          range: true,
+          min: 0,
+          max: 40,
+          values: [10, 35],
+          slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+          }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+          " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+      }
+    );
+    </script>
+
     <style>
-        /* Remove the navbar's default rounded borders and increase the bottom margin */
         .navbar {
             margin-bottom: 50px;
             border-radius: 0;
         }
 
-        /* Remove the jumbotron's default bottom margin */
         .jumbotron {
             margin-bottom: 0;
         }
 
-        /* Add a gray background color and some padding to the footer */
         footer {
             background-color: #f2f2f2;
             padding: 25px;
         }
     </style>
 </head>
+
 <body>
-
-<div class="jumbotron">
-    <div class="container text-center">
-        <h1>Welcome to Banana World</h1>
-        <p>Here you can find all sorts of bananas !</p>
-        <img class="minion" src="https://img0.etsystatic.com/100/0/10775770/il_340x270.832626606_imki.jpg" alt="Un minion avec une banane"/>
-    </div>
-</div>
-
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-left" style="background-color:white">
+              <form class="form-inline" role="form" method="post">
+                  <div class="form-group">
+                      <label class="filter-col" style="margin-right:0;" for="pref-perpage">Category :</label>
+                      <select name ="cat" id="pref-perpage" class="form-control">
+                        <option value="All">All</option>
+                        <?php
+                           foreach (get_categories($db) as $cat) {
+                               echo '<option value='.$cat->name.'>'.$cat->name.'</option>';
+                           }
+                        ?>
+                      </select>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="filter-col" style="margin-right:0;" for="pref-search">Search:</label>
+                      <input name ="name" type="text" class="form-control input-sm" id="pref-search">
+                  </div>
+
+                  <div class="form-group">
+                    <label class="min">Min Price:</label>
+                    <input name ="min" type="number">
+                  </div>
+
+                  <div class="form-group">
+                    <label class="max">Max Price:</label>
+                    <input name ="max" type="number">
+                  </div>
+
+                  <div class="form-group">
+                      <label class="filter-col" style="margin-right:0;" for="pref-orderby">Order by:</label>
+                      <select name="order" id="pref-orderby" class="form-control">
+                          <option>None</option>
+                          <option>Ascending Price</option>
+                          <option>Descending Price</option>
+                          <option>Ascending Alphabetical</option>
+                          <option>Descending Alphabetical</option>
+                      </select>
+
+                  </div>
+
+                  <div class="form-group">
+                      <button type="submit" class="btn btn-default filter-col">
+                          <span class="glyphicon glyphicon-refresh"></span> Refresh
+                      </button>
+                  </div>
+
+              </form>
+            </ul>
             <ul class="nav navbar-nav navbar-right">
               <?php
-                if(isset($_SESSION['username'])){
-                  echo'<li><a><span class="glyphicon glyphicon-user"></span>'.$_SESSION['username'].'</a></li>
-                  <li><a href="index.php?logout=1">Logout</a></li>';
-                }
-                else{
-                  echo'<li><a href="User/Login_Register/login.php" ><span class="glyphicon glyphicon-user"></span>  Log in</a></li>';
+                if (isset($_SESSION['username'])) {
+                    echo'<li><a><span class="glyphicon glyphicon-user"></span>'.$_SESSION['username'].'</a></li>
+                  <li><a href="User/Login_Register/login.php">Logout</a></li>';
+                } else {
+                    echo'<li><a href="User/Login_Register/login.php" ><span class="glyphicon glyphicon-user"></span>  Log in</a></li>';
                 }
               ?>
 
@@ -75,17 +126,75 @@ if (isset($_GET['logout'])) {
 <div class="container">
     <div class="row">
       <?php
-        foreach(get_products($db) as $prod){
+      $data = [
+      'name' => $_POST['cat'],
+      ];
+      $stmt = $db->prepare("SELECT id FROM categories WHERE name = :name
+      ");
+      $stmt->execute($data);
+      $element = $stmt->fetchAll(PDO::FETCH_OBJ);
+      if ($element[0]){
+        $catid = $element[0]->id;
+      }
+      else{
+        $catid='%';
+      }
 
-          $data = [
+
+
+      switch ($_POST['order']) {
+        case 'Ascending Price':
+            $order="ASC";
+            $way="price";
+          break;
+
+        case 'Descending Price':
+            $order="DESC";
+            $way="price";
+          break;
+
+        case 'Ascending Alphabetical':
+              $order="ASC";
+              $way="name";
+            break;
+
+        case 'Descending Alphabetical':
+              $order="DESC";
+              $way="name";
+            break;
+
+        default:
+          $order="ASC";
+          $way="name";
+          break;
+      }
+
+      if ($_POST['name']==NULL){
+        $_POST['name']='%';
+      }
+
+      if ($_POST['min']==NULL){
+        $_POST['min']='10';
+      }
+
+      if ($_POST['max']==NULL){
+        $_POST['max']='30';
+      }
+
+      if(count(search_product($db,  $catid,$_POST['name'],$_POST['min'],$_POST['max'],$order,$way))>0){
+        foreach (search_product($db,1,$_POST['name'],$_POST['min'],$_POST['max'],$order,$way) as $prod) {
+            $data = [
             'id' => $prod->category_id,
           ];
-
-          $stmt = $db->prepare("SELECT name FROM categories WHERE id = :id");
-          $stmt->execute($data);
-          $cat = $stmt->fetch(PDO::FETCH_OBJ);
-          $form = new product($prod->name,"index.php", $prod->image ,$cat->name,$prod->price);
+            $stmt = $db->prepare("SELECT name FROM categories WHERE id = :id");
+            $stmt->execute($data);
+            $cat = $stmt->fetch(PDO::FETCH_OBJ);
+            $form = new product($prod->name, "index.php", $prod->image, $cat->name, $prod->price);
         }
+      }
+      else{
+        echo "No results found";
+      }
       ?>
 
 
